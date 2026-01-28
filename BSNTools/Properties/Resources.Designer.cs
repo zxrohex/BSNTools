4f5abb6259c3ed7a7631cf6e56653d95f862fe97
zxrohex <contact@bytestxck.software>
2026-01-28 20:33:36 +0100

    Experimental branch commit

--- BSNTools/Properties/Resources.Designer.cs
+++ BSNTools/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace BSNTools.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace BSNTools.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BSNTools.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace BSNTools.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace BSNTools.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap BSNToolsLogo {
+        public static System.Drawing.Bitmap BSNToolsLogo {
             get {
                 object obj = ResourceManager.GetObject("BSNToolsLogo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -73,7 +73,7 @@ namespace BSNTools.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap BSNToolsLogoNoBG {
+        public static System.Drawing.Bitmap BSNToolsLogoNoBG {
             get {
                 object obj = ResourceManager.GetObject("BSNToolsLogoNoBG", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
